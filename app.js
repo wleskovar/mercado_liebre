84816fe27bf19678c9574c63214c73ca06a15c77
wleskovar <wleskovar@gmail.com>
2022-03-30 16:06:32 -0300

    primer commit Estructura de un sitio web

---
+++ app.js
@@ -0,0 +1,24 @@
+const express= require('express');
+const path= require('path');
+
+const app= express();
+
+const public= path.resolve(__dirname, './public');
+const views= path.resolve(__dirname, './views');
+
+// console.clear();
+// console.log(public);
+// console.log(views);
+
+app.use(express.static(public));
+
+
+app.listen(8000, ()=> {
+    console.log('Se inicio el servidor en el puerto 8000')
+});
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(views + '/home.html');
+})
+
+
